fec72b6d6926852c201a8b9e58794f648f6f55e6
hachiman770 <hachiman270@gmail.com>
2023-08-12 09:18:01 +0100

    blk-core: Use local_bh disable/enable in __blk_run_queue_uncond()
    Signed-off-by: hachiman770 <hachiman270@gmail.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -381,9 +381,9 @@ inline void __blk_run_queue_uncond(struct request_queue *q)
 	 * can wait until all these request_fn calls have finished.
 	 */
 	q->request_fn_active++;
-	preempt_disable();
+	local_bh_disable();
 	q->request_fn(q);
-	preempt_enable();
+	local_bh_enable();
 	q->request_fn_active--;
 }
 EXPORT_SYMBOL_GPL(__blk_run_queue_uncond);
